d39bcfbe527e7ebc383ed115aea3cc9b990b438b
bin0d12 <76993936+bin0d12@users.noreply.github.com>
2021-01-30 08:01:11 +0530

    Add files via upload

--- drum-kit.js
+++ drum-kit.js
@@ -16,3 +16,44 @@ function removeTransition(e) {
   const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown', playSound);
+
+
+
+// function playSound(e){
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//     if(!audio) return; // stop the function from running the all together
+//     audio.currentTime = 0; //rewind to the start
+//     audio.play();
+//     key.classList.add('playing');
+// }
+// function removeTransition(e){
+//     if (e.propertyName !== 'transform') return;
+//     this.classList.remove('playing');
+// }
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(key => key.addEventlistener('transitionend', removeTransition));
+// window.addEventListener('keydown', playSound);
+
+
+
+
+
+
+
+//   function playSound(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//     if (!audio) return;
+// key.classList.add('playing');
+//     audio.currentTime = 0;
+//     audio.play();
+//   }
+//   function removeTransition(e) {
+//     if (e.propertyName !== 'transform') return;
+//     e.target.classList.remove('playing');
+//   }
+
+//   const keys = Array.from(document.querySelectorAll('.key'));
+//   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//   window.addEventListener('keydown', playSound);
